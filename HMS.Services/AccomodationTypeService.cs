76dc518a93b6f1c03ed338bb1a17193d6245bf68
Krishna Rana <info2krishnarana@gmail.com>
2019-06-30 16:40:33 +0530

    Accomodation Types listing and create

---
+++ HMS.Services/AccomodationTypeService.cs
@@ -0,0 +1,25 @@
+﻿using HMS.Data;
+using HMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Services
+{
+    public class AccomodationTypeService
+    {
+        HMSContext context = new HMSContext();
+        public IEnumerable<AccomodationType> GetAllAccomodationTypes()
+        {
+            return context.AccomodationTypes.ToList();
+        }
+
+        public bool SaveAccomodationType(AccomodationType accomodationType)
+        {
+            context.AccomodationTypes.Add(accomodationType);
+            return context.SaveChanges() > 0;
+        }
+    }
+}
---
+++ HMS/Areas/Dashboard/Controllers/AccomodationTypesController.cs
@@ -0,0 +1,57 @@
+﻿using HMS.Areas.Dashboard.ViewModels;
+using HMS.Entities;
+using HMS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HMS.Areas.Dashboard.Controllers
+{
+    public class AccomodationTypesController : Controller
+    {
+        AccomodationTypeService accomodationTypeService = new AccomodationTypeService();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listing()
+        {
+            AccomodationTypeViewModel model = new AccomodationTypeViewModel();
+
+            model.AccomodationTypes = accomodationTypeService.GetAllAccomodationTypes();
+            return PartialView("_Listing", model);
+        }
+
+        [HttpGet]
+        public ActionResult Action()
+        {
+            AccomodationTypeActionViewModel model = new AccomodationTypeActionViewModel();
+            return PartialView("_Action", model);
+        }
+
+        [HttpPost]
+        public JsonResult Action(AccomodationTypeActionViewModel model)
+        {
+            JsonResult jsonResult = new JsonResult();
+
+            AccomodationType accomodationType = new AccomodationType();
+            accomodationType.Name = model.Name;
+            accomodationType.Description = model.Description;
+
+            var result = accomodationTypeService.SaveAccomodationType(accomodationType);
+            if (result)
+            {
+                jsonResult.Data = new { Success = true };
+            }
+            else
+            {
+                jsonResult.Data = new { Success = false, Message = "Enable to add Accomodation Tpype" };
+            }
+            return jsonResult;
+        }
+    }
+}
